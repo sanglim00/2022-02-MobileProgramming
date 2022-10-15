c6645bf5400af5ff178d3123ba4c56be37a85e3f
sanglim <sanglim@kookmin.ac.kr>
2022-10-15 16:49:23 +0900

    feat. 상품페이지 수정 중

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -8,12 +8,22 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button JoinBtn;
+    Button ProductBtn, JoinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProductBtn = (Button) findViewById(R.id.productBtn);
+        ProductBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         JoinBtn = (Button) findViewById(R.id.joinBtn);
         JoinBtn.setOnClickListener(new View.OnClickListener() {
