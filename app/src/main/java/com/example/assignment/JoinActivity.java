0131528c91cbaf689532856d67c95cd7c192392d
sanglim <sanglim@kookmin.ac.kr>
2022-10-17 13:18:22 +0900

    feat. 회원가입 수정중

--- app/src/main/java/com/example/assignment/JoinActivity.java
+++ app/src/main/java/com/example/assignment/JoinActivity.java
@@ -6,14 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class JoinActivity extends AppCompatActivity {
     Button backBtn;
+    private EditText joinID, joinPW, joinPW2, joinName, joinPhone, joinAddress ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        joinID = (EditText) findViewById(R.id.joinID);
+        joinPW = (EditText) findViewById(R.id.joinPW);
+//        joinPW2 = (EditText) findViewById(R.id.joinPW2);
+        joinName = (EditText) findViewById(R.id.joinName);
+        joinPhone = (EditText) findViewById(R.id.joinPhone);
+        joinAddress = (EditText) findViewById(R.id.joinAddress);
+
 
         backBtn = (Button) findViewById(R.id.backBtn);
         backBtn.setOnClickListener(new View.OnClickListener() {
