3ee367976a58e9eacabe915118a8dcd5b2fa272a
sanglim <sanglim@kookmin.ac.kr>
2022-10-29 19:11:20 +0900

    feat. 기능 수정 중

--- app/src/main/java/com/example/assignment/JoinActivity.java
+++ app/src/main/java/com/example/assignment/JoinActivity.java
@@ -9,15 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class JoinActivity extends AppCompatActivity {
     Button checkIDBtn, joinFinishBtn,  backBtn;
     private EditText joinID, joinPW, rejoinPW, joinName, joinPhone, joinAddress ;
-    private RadioButton ppRadio;
+    private RadioGroup ppRadio;
 
-    SharedPreferences spref;
-    SharedPreferences.Editor editor;
+    boolean checkID = false, checkPW = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +38,22 @@ public class JoinActivity extends AppCompatActivity {
         checkIDBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences prefs = getSharedPreferences("user_info", 0);
-                SharedPreferences.Editor editor = prefs.edit();
+                SharedPreferences sharedPreferences= getSharedPreferences("user_info", 0);
+                String id = sharedPreferences.getString("userID","");
+                joinID = (EditText) findViewById(R.id.joinID);
+
+                if(joinID.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "아이디를 입력해주세요", Toast.LENGTH_SHORT).show();
+                    checkID = false;
+                }
+                else if(joinID.getText().toString().equals(id)) {
+                    Toast.makeText(getApplicationContext(), "이미 존재하는 아이디입니다. 다른 아이디를 설정해주세요", Toast.LENGTH_SHORT).show();
+                    checkID = false;
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT).show();
+                    checkID = true;
+                }
 
             }
         });
@@ -54,7 +69,14 @@ public class JoinActivity extends AppCompatActivity {
                 joinPhone = (EditText) findViewById(R.id.joinPhone);
                 joinAddress = (EditText) findViewById(R.id.joinAddress);
 
-                if(savedInstanceState == null) {
+                if(joinPW.getText().toString().equals(rejoinPW.getText().toString())) checkPW = true;
+                else {
+                    Toast.makeText(getApplicationContext(), "두 비밀번호가 다릅니다", Toast.LENGTH_SHORT).show();
+                    checkPW = false;
+                    return ;
+                }
+
+                if(checkPW && checkID) {
                     SharedPreferences prefs = getSharedPreferences("user_info", 0);
                     SharedPreferences.Editor editor = prefs.edit();
 
@@ -76,6 +98,12 @@ public class JoinActivity extends AppCompatActivity {
 
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
+                }
+                else if (!checkID) {
+                    Toast.makeText(getApplicationContext(), "아이디를 확인하세요", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "입력을 다시 확인하세요", Toast.LENGTH_SHORT).show();
                 }
             }
         });
