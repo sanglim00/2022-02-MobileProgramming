0bb636584676737e5be59836742e2afac122e62f
sanglim <sanglim@kookmin.ac.kr>
2022-10-29 19:29:55 +0900

    feat. 기능 수정 중

--- app/src/main/java/com/example/assignment/JoinActivity.java
+++ app/src/main/java/com/example/assignment/JoinActivity.java
@@ -10,10 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class JoinActivity extends AppCompatActivity {
-    Button checkIDBtn, joinFinishBtn,  backBtn;
+    Button checkIDBtn, joinFinishBtn, backBtn;
     private EditText joinID, joinPW, rejoinPW, joinName, joinPhone, joinAddress ;
     private RadioGroup ppRadio;
 
@@ -25,7 +26,7 @@ public class JoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
-
+        // 뒤로가기 버튼 클릭 시 다시 첫 화면으로
         backBtn = (Button) findViewById(R.id.backBtn);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +35,7 @@ public class JoinActivity extends AppCompatActivity {
             }
         });
 
+        // 아이디 중복확인
         checkIDBtn = (Button) findViewById(R.id.checkIDBtn);
         checkIDBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,23 +43,30 @@ public class JoinActivity extends AppCompatActivity {
                 SharedPreferences sharedPreferences= getSharedPreferences("user_info", 0);
                 String id = sharedPreferences.getString("userID","");
                 joinID = (EditText) findViewById(R.id.joinID);
+                TextView idCheckMsg = (TextView) findViewById(R.id.idCheckMsg);
 
+                // 입력값이 비어있을 경우
                 if(joinID.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "아이디를 입력해주세요", Toast.LENGTH_SHORT).show();
+                    idCheckMsg.setText("아이디를 입력해주세요");
+                    idCheckMsg.setVisibility(View.VISIBLE);
                     checkID = false;
                 }
+                // 이미 존재하는 아이디일 경우
                 else if(joinID.getText().toString().equals(id)) {
-                    Toast.makeText(getApplicationContext(), "이미 존재하는 아이디입니다. 다른 아이디를 설정해주세요", Toast.LENGTH_SHORT).show();
+                    idCheckMsg.setText("이미 존재하는 아이디입니다. 다른 아이디를 설정해주세요");
+                    idCheckMsg.setVisibility(View.VISIBLE);
                     checkID = false;
                 }
                 else {
-                    Toast.makeText(getApplicationContext(), "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT).show();
+                    idCheckMsg.setText("사용 가능한 아이디입니다.");
+                    idCheckMsg.setVisibility(View.VISIBLE);
                     checkID = true;
                 }
 
             }
         });
 
+        // 회원가입 하기 버튼 클릭 시
         joinFinishBtn = (Button) findViewById(R.id.joinFinishBtn);
         joinFinishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +78,7 @@ public class JoinActivity extends AppCompatActivity {
                 joinPhone = (EditText) findViewById(R.id.joinPhone);
                 joinAddress = (EditText) findViewById(R.id.joinAddress);
 
+                // 비밀번호 입력 체크
                 if(joinPW.getText().toString().equals(rejoinPW.getText().toString())) checkPW = true;
                 else {
                     Toast.makeText(getApplicationContext(), "두 비밀번호가 다릅니다", Toast.LENGTH_SHORT).show();
@@ -76,6 +86,7 @@ public class JoinActivity extends AppCompatActivity {
                     return ;
                 }
 
+                // 회원가입
                 if(checkPW && checkID) {
                     SharedPreferences prefs = getSharedPreferences("user_info", 0);
                     SharedPreferences.Editor editor = prefs.edit();
@@ -99,9 +110,11 @@ public class JoinActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                 }
+                // 아이디 중복 확인을 하지 않았을 때
                 else if (!checkID) {
                     Toast.makeText(getApplicationContext(), "아이디를 확인하세요", Toast.LENGTH_SHORT).show();
                 }
+                // 입력이 제대로 되어있지 않았을 때
                 else {
                     Toast.makeText(getApplicationContext(), "입력을 다시 확인하세요", Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -39,16 +39,17 @@ public class MainActivity extends AppCompatActivity {
         userID.setText(id);
         userPW.setText(pw);
 
-
+        // 로그인 버튼 클릭 시
         loginBtn = (Button) findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // 입력값과 저장된 값이 같을경우
                 if(userID.getText().toString().equals(id) && userPW.getText().toString().equals(pw)) {
                     Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
                     startActivity(intent);
                 }
+                // 입력값과 다를 경우
                 else {
                     Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show();
                 }
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // 회원가입 버튼 클릭 시 회원가입 페이지로 이동
         JoinBtn = (Button) findViewById(R.id.joinBtn);
         JoinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
