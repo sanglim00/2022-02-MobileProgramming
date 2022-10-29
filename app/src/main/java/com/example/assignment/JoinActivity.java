6dfdf8079e1f3018bd64605fd706cee8c667dc84
sanglim <sanglim@kookmin.ac.kr>
2022-10-29 16:37:03 +0900

    feat. 회원가입 기능 구현 중

--- app/src/main/java/com/example/assignment/JoinActivity.java
+++ app/src/main/java/com/example/assignment/JoinActivity.java
@@ -8,11 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class JoinActivity extends AppCompatActivity {
-    Button backBtn;
+    Button checkIDBtn, joinFinishBtn,  backBtn;
     private EditText joinID, joinPW, rejoinPW, joinName, joinPhone, joinAddress ;
+    private RadioButton ppRadio;
+
+    SharedPreferences spref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,55 +32,55 @@ public class JoinActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        checkIDBtn = (Button) findViewById(R.id.checkIDBtn);
+        checkIDBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences prefs = getSharedPreferences("user_info", 0);
+                SharedPreferences.Editor editor = prefs.edit();
+
+            }
+        });
+
+        joinFinishBtn = (Button) findViewById(R.id.joinFinishBtn);
+        joinFinishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinID = (EditText) findViewById(R.id.joinID);
+                joinPW = (EditText) findViewById(R.id.joinPW);
+                rejoinPW = (EditText) findViewById(R.id.reJoinPW);
+                joinName = (EditText) findViewById(R.id.joinName);
+                joinPhone = (EditText) findViewById(R.id.joinPhone);
+                joinAddress = (EditText) findViewById(R.id.joinAddress);
+
+                if(savedInstanceState == null) {
+                    SharedPreferences prefs = getSharedPreferences("user_info", 0);
+                    SharedPreferences.Editor editor = prefs.edit();
+
+                    String userID = joinID.getText().toString();
+                    String userPW = joinPW.getText().toString();
+                    String reUserPW = rejoinPW.getText().toString();
+                    String userName = joinName.getText().toString();
+                    String userPhone = joinPhone.getText().toString();
+                    String userAddress = joinAddress.getText().toString();
+
+
+                    editor.putString("userID", userID);
+                    editor.putString("userPW", userPW);
+                    editor.putString("reUserPW", reUserPW);
+                    editor.putString("userName", userName);
+                    editor.putString("userPhone", userPhone);
+                    editor.putString("userAddress", userAddress);
+
+                    editor.apply();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 
-    public void setUserData(View view) {
-        joinID = (EditText) findViewById(R.id.joinID);
-        joinPW = (EditText) findViewById(R.id.joinPW);
-        rejoinPW = (EditText) findViewById(R.id.reJoinPW);
-        joinName = (EditText) findViewById(R.id.joinName);
-        joinPhone = (EditText) findViewById(R.id.joinPhone);
-        joinAddress = (EditText) findViewById(R.id.joinAddress);
-
-        SharedPreferences prefs = getSharedPreferences("user_info", 0);
-        SharedPreferences.Editor editor = prefs.edit();
-
-        String userID = joinID.getText().toString();
-        String userPW = joinPW.getText().toString();
-        String reUserPW = rejoinPW.getText().toString();
-        String userName = joinName.getText().toString();
-        String userPhone = joinPhone.getText().toString();
-        String userAddress = joinAddress.getText().toString();
-
-        if(userID == null) {
-            Toast.makeText(getApplicationContext(), "아이디를 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else if(userPW == null) {
-            Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else if(reUserPW == null) {
-            Toast.makeText(getApplicationContext(), "비밀번호를 다시 한 번 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else if(userName == null) {
-            Toast.makeText(getApplicationContext(), "이름을 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else if(userPhone == null) {
-            Toast.makeText(getApplicationContext(), "번호를 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else if(userAddress == null) {
-            Toast.makeText(getApplicationContext(), "주소를 입력해주세요", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            editor.putString("userID", userID);
-            editor.putString("userPW", userPW);
-            editor.putString("reUserPW", reUserPW);
-            editor.putString("userName", userName);
-            editor.putString("userPhone", userPhone);
-            editor.putString("userAddress", userAddress);
-
-            editor.apply();
-        }
-
-
-    }
 }
