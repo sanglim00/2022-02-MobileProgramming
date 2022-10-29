cb8c3c67c3c0406dfd1d117a5df1641394b0a0cd
sanglim <sanglim@kookmin.ac.kr>
2022-10-29 20:38:53 +0900

    feat. 다이얼로그 띄우기

--- app/src/main/java/com/example/assignment/ProductActivity.java
+++ app/src/main/java/com/example/assignment/ProductActivity.java
@@ -1,8 +1,12 @@
 package com.example.assignment;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -12,5 +16,24 @@ public class ProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product);
 
 
+    }
+    public void DialogClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("내 정보").setMessage("내 정보");
+        builder.setPositiveButton("회원가입하기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+//                Toast.makeText(getApplicationContext(),"닫기 버튼 클릭", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
