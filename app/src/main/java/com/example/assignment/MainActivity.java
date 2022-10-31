0c3b7def49bf8f63ba48f3fb2487ac0d70db14b9
sanglim <sanglim@kookmin.ac.kr>
2022-11-01 02:21:37 +0900

    feat. 프레퍼런스로 유저관리

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     TextView UserState ;
 
     Boolean LoginCheck = false;
+    Boolean checkUser = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +40,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        SharedPreferences sharedPreferences= getSharedPreferences("user_info", 0);
-        String id = sharedPreferences.getString("userID","");
-        String pw = sharedPreferences.getString("userPW", "");
 
-        userID = (EditText) findViewById(R.id.userID);
-        userPW = (EditText) findViewById(R.id.userPW);
-        userID.setText(id);
-        userPW.setText(pw);
 
         // 로그인 버튼 클릭 시
         loginBtn = (Button) findViewById(R.id.loginBtn);
@@ -53,12 +48,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                userID = (EditText) findViewById(R.id.userID);
+                userPW = (EditText) findViewById(R.id.userPW);
+
+                SharedPreferences sharedPreferences= getSharedPreferences("user_info", 0);
+                int savedCount = sharedPreferences.getInt("count", 0);
+
+                // 저장된 아이디와 비밀번호가 모두 일치하는 지 확인
+                for (int i =1; i <= savedCount; i++) {
+                    String id = sharedPreferences.getString("userID"+i, "");
+                    String pw = sharedPreferences.getString("userPW"+i, "");
+                    // 반복문 중 하나의 계정이라도 일치하면 checkUser를 true로 바꾸고 반복문 정지
+                    if (userID.getText().toString().equals(id) && userPW.getText().toString().equals(pw) ){
+                        checkUser = true;
+                        break;
+                    }
+                }
+
                 if(userID.getText().toString().isEmpty() || userPW.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 제대로 입력해주세요", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                Toast.makeText(getApplicationContext(), checkUser.toString(), Toast.LENGTH_SHORT).show();
+
                 // 입력값과 저장된 값이 같을경우
-                if(userID.getText().toString().equals(id) && userPW.getText().toString().equals(pw)) {
+                if(checkUser == true) {
                     LoginCheck = true;
                     UserState.setText("회원 상태입니다.");
                     Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
